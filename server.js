d80e341dd6b7a3ee85e0ca92ef862fc671825b02
Mikel1991 <mikel362d@gmail.com>
2020-06-11 17:46:31 -0700

    set up get and post apis and print 1 object into db.json

--- server.js
+++ server.js
@@ -15,20 +15,43 @@ app.use(express.json());
 //Set empty array to store notes
 let notes = [];
 
-//Routes
+//API routes
+app.get("/api/notes", (req, res) => {
+    // reads the notes from json file
+    fs.readFile(__dirname + "/db/db.json", (err, data) => { 
+        if (err) throw err;
+        //parse the buffered data on JSON format
+        let noteData = JSON.parse(data)
+        res.json(noteData);
+    });
+});
+
+app.post("/api/notes", (req, res) => {
+    //Get access to db.json
+    fs.readFile(__dirname + "/db/db.json", (err, data) => {if (err) throw err});
+    //Store new note in newNote variable
+    let newNote = req.body;
+    //Push newNote to the array of notes
+    notes.push(newNote);
+    // Stringify array
+    notes = JSON.stringify(notes);
+    //Write new array of notes 
+    fs.writeFile(__dirname + "/db/db.json", notes, (err, data) => {if (err) throw err }); 
+    console.log(notes);
+    console.log(newNote);
+    res.json(JSON.parse(notes));
+
+});
+
+
+//HTML Routes
 app.get("/notes", (req, res) => res.sendFile(__dirname + "/public/notes.html"));
 
 app.get("*", (req, res) => res.sendFile(__dirname + "/public/index.html"));
 
-//API routes
-app.get("/api/notes", (req, res) => {
-    // reads the notes from json file
-    const noteData = fs.readFile("/db/db.json", "utf8");
-    console.log(notes);
-    res.json(noteData);
-});
 
 // Start the server on the port
 app.listen(PORT, function() {
     console.log("SERVER IS LISTENING ON PORT: " + PORT);
   });
+
