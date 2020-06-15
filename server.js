fa176c043464737e4a8d55917daad1d59644014f
Mikel1991 <mikel362d@gmail.com>
2020-06-15 15:26:55 -0700

    package.json start script added

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.delete("/api/notes/:id", (req, res) => {
 //HTML Routes
 app.get("/notes", (req, res) => res.sendFile(__dirname + "/public/notes.html"));
 
-app.get("*", (req, res) => res.sendFile(__dirname + "/public/index.html"));
+app.get("/", (req, res) => res.sendFile(__dirname + "/public/index.html"));
 
 //CSS Route
 app.get("/notes", (req, res) => res.sendFile(__dirname + "/public/assets/css/styles.css"));
